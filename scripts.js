8df5b4859aa0bc0ae07e97fd6bee81624008bfea
Jonathan Szkup <jszkup101@gmail.com>
2024-02-20 19:12:32 -0500

    add notes, basic operation functions

---
+++ scripts.js
@@ -0,0 +1,47 @@
+// TODO should they all accept a list as an argument?
+const add = function (a, b) {
+    return a + b;
+}
+
+const subtract = function (a, b) {
+    return a - b;
+}
+
+const multiply = function (a, b) {
+    return a * b;
+}
+
+const divide = function (a, b) {
+    return a / b;
+}
+
+let operation = function () {
+    let operand;
+    let operand2;
+    let operator;
+    // pulls variables from html buttons
+
+    return, operand, operand2, operator;
+
+}
+
+function operate(operator, operator2, operand) {
+    // if operand(+) run add function...
+
+}
+
+// const multiply = function (numList) {
+//     let result = numList.reduce((total, current) => total * current, 1);
+
+//     return result;
+// }
+// const divide = function (numList) {
+//     let result = numList.reduce((total, current) => total / current, 1);
+
+//     return result;
+// }
+
+// operator and numbers are separate variables
+
+// calculation function should sit in a reduce()
+// within the reduce, different functions should decide PEMDAS
